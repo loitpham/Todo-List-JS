8336b2d11c0c0ad127879308fb27664a16cd5674
Loi Pham <loitpham@gmail.com>
2021-04-22 10:04:18 -0700

    Added searching and filtering functionality.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
 
 const generateTemplate = todo => {
     const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
@@ -16,4 +17,28 @@ addForm.addEventListener('submit', e => {
         generateTemplate(todo);
         addForm.reset();
     }
+});
+
+// delete todos
+list.addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+});
+
+const filterTodos = term => {
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+
+    // Need to remove class 'filtered' for those previously filtered
+    Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+// keyup event
+search.addEventListener('keyup', e => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
 });
